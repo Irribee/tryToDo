b811c90a55934d6d2c2aea62a633bd5d779c30f7
Irribee <75831282+Irribee@users.noreply.github.com>
2023-01-31 00:12:39 +0300

    добавлены репозитории

--- src/main/java/com/irribee/repository/ToDoRepository.java
+++ src/main/java/com/irribee/repository/ToDoRepository.java
@@ -1,8 +1,18 @@
 package com.irribee.repository;
 
 import com.irribee.entity.TaskEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ToDoRepository extends CrudRepository<TaskEntity, Integer> {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
+@Repository
+public class ToDoRepository {
+    private static final Map<Integer, TaskEntity> toDoMap = new HashMap<>();
+
+
+    public Optional<TaskEntity> findById(int id) {
+        return Optional.ofNullable(toDoMap.get(id));
+    }
 }
